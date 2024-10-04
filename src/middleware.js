0d3158e88a9b7de60b0dd1312c340f3fb0aefef1
Stylo <136652260+stylo-codes-stuff@users.noreply.github.com>
2024-10-04 20:03:04 +0000

    work in progress request interception

---
+++ src/middleware.js
@@ -0,0 +1,27 @@
+
+//request interception and handling 
+function response_handler(req,res,next){
+    //original intact send func
+    const og_send = res.send;
+
+    //func override for reponse body modification
+    res.send = function(body){
+        const response_body = body +"test"
+        og_send.call(this,response_body)
+        };
+    next();
+    }
+
+function json_handler(req,res,next){
+    //original intact json function
+    const og_json = res.json;
+
+    res.json = function(body) {
+        const json_response_body = {...body}
+        og_json.call(this,json_response_body + "test")
+    }
+    next();
+}
+
+exports.json_handler = json_handler;
+exports.response_handler = response_handler
--- src/server.js
+++ src/server.js
@@ -1,1 +1,34 @@
+const {json_handler,response_handler} = require('./middleware.js')
+const express = require('express');
+const https = require("node:https");
+const app = express();
+const port = 3000;
 
+app.use(response_handler)
+app.use(json_handler)
+//homepage endpoint
+app.get('/', (req, res) => {
+  res.send('this is the root for this server')
+});
+//debugging endpoint
+
+app.get('/request/:URL',(req,res) => {
+    url = req.params
+    res.send(url[URL])
+    https.get(url, (req_response) => {
+
+      console.log('statusCode:', req_response.statusCode);
+      console.log('headers:', req_response.headers);
+    
+      req_response.on('data', (d) => {
+        res.send(d)
+      });
+    
+    }).on('error', (e) => {
+      console.error(e);
+    });
+    res.send()    
+});
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
